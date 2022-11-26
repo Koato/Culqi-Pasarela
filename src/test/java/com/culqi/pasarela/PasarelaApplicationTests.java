bf86218e14b450e408a89a642d4fc7bb8cfdec16
Andy Gómez Ordinola <45979588+Koato@users.noreply.github.com>
2022-11-25 19:24:57 -0500

    Update PasarelaApplicationTests.java

--- src/test/java/com/culqi/pasarela/PasarelaApplicationTests.java
+++ src/test/java/com/culqi/pasarela/PasarelaApplicationTests.java
@@ -1,13 +1,33 @@
 package com.culqi.pasarela;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import com.culqi.pasarela.controller.TokenController;
+import com.culqi.pasarela.services.ITokenServices;
+
+
+import static org.mockito.Mockito.when;
+
+@WebMvcTest(TokenController.class)
 class PasarelaApplicationTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+
+	@MockBean
+	private ITokenServices service;
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void executeToken() {
+		when(service.generateToken()).thenReturn("DMJz2QjuZLpeaO6J");
 	}
 
 }
